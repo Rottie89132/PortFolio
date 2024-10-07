2a42d60bd0b3c3aa1f5d4a45de89bdf32f5aca7b
Rottie#7658 <89132@glr.nl>
2024-10-07 18:30:01 +0200

    padding gefixed

--- components/card/github.vue
+++ components/card/github.vue
@@ -5,27 +5,20 @@
 				<NuxtImg draggable="false" :src="item.owner.avatar_url" class="w-5 rounded-full"> </NuxtImg>
 				<h3 class="font-semibold dark:text-white">{{ item.full_name }}</h3>
 			</div>
-			<p
-				class="mt-3 md:mt-1 whitespace-pre-wrap text-balance dark:text-white mr-5 text-sm break-words opacity-70 line-clamp-2">
+			<p class="mt-3 md:mt-1 whitespace-pre-wrap text-balance dark:text-white mr-5 text-sm break-words opacity-70 line-clamp-2">
 				{{ item.description || defaultDescription }}
 			</p>
 			<div class="flex items-center gap-3 mt-3 mb-1 font-semibold">
-				<button v-if="item.saved" @click="DeleteRepo(item)"
-					class="flex items-center justify-center font-semibold px-3 py-1 text-xs dark:text-black dark:bg-white dark:ring-white text-white bg-black rounded ring-2 ring-black">Verwijderen</button>
-				<button v-else @click="SaveRepo(item)"
-					class="flex items-center justify-center px-3 py-1 font-semibold text-xs dark:text-black dark:bg-white dark:ring-white text-white bg-black rounded ring-2 ring-black">Toevoegen</button>
+				<button v-if="item.saved" @click="DeleteRepo(item)" class="flex items-center justify-center font-semibold px-3 py-1 text-xs dark:text-black dark:bg-white dark:ring-white text-white bg-black rounded ring-2 ring-black">Verwijderen</button>
+				<button v-else @click="SaveRepo(item)" class="flex items-center justify-center px-3 py-1 font-semibold text-xs dark:text-black dark:bg-white dark:ring-white text-white bg-black rounded ring-2 ring-black">Toevoegen</button>
 
 				<button v-if="item.saved" @click="UpdateRepo(item)" class="p-[0.33em] rounded-md text-xs flex justify-center items-center font-semibold dark:text-neutral-300 dark:bg-neutral-800 dark:ring-neutral-800 text-neutral-600 bg-neutral-200 ring-2 ring-neutral-200">
 					<Icon name="material-symbols:feature-search" size="1.4em"></Icon>
 				</button>
 
-
-				<button v-if="item.open_issues_count > 0"
-					class="p-[0.33em] rounded-md text-xs flex justify-center items-center font-semibold dark:text-neutral-300 dark:bg-neutral-800 dark:ring-neutral-800 text-neutral-600 bg-neutral-200 ring-2 ring-neutral-200">
-					<NuxtLink :to="`/dashboard/repos/issues/${item.name}`">
-						<Icon name="material-symbols:release-alert" size="1.4em"></Icon>
-					</NuxtLink>
-				</button>
+				<NuxtLink :to="`/dashboard/repos/issues/${item.name}`" v-if="item.open_issues_count > 0" class="p-[0.33em] rounded-md text-xs flex justify-center items-center font-semibold dark:text-neutral-300 dark:bg-neutral-800 dark:ring-neutral-800 text-neutral-600 bg-neutral-200 ring-2 ring-neutral-200">
+					<Icon name="material-symbols:release-alert" size="1.4em"></Icon>
+				</NuxtLink>
 			</div>
 		</div>
 	</div>
