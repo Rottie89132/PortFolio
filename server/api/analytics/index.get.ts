c86578504383f16ee30fb82f8101087d578d0f4f
Rottie#7658 <89132@glr.nl>
2024-11-03 00:11:26 +0100

    filter fix voor analytic en nitropack vast gezet op v2.9.7 tot dat nuxt-surf werkt met v2.10 en meer icon packs geinstaleerd

--- server/api/analytics/index.get.ts
+++ server/api/analytics/index.get.ts
@@ -2,9 +2,7 @@ export default defineEventHandler(async (event) => {
     return new Promise( async(resolve, reject) => {
         const SessionId: any = getCookie(event, "access-token")
         const user: Record<string, any> | null = await useStorage("Sessions").getItem(SessionId)
-        const NowMonth: number = new Date().getMonth() + 1
-        const { today }: any = getQuery(event)
-
+    
         if (!user) return reject({
             statusCode: 401,
             statusMessage: "Unauthorized",
@@ -17,18 +15,11 @@ export default defineEventHandler(async (event) => {
             message: "The server understood the request but refuses to authorize it."
         })
 
-        if (today) return resolve({
-            statusCode: 200,
-            statusMessage: "OK",
-            message: "The request has succeeded.",
-            result: await analytics.findOne({ CurrentMonth: NowMonth })
-        })
-
         return resolve({
             statusCode: 200,
             statusMessage: "OK",
             message: "The request has succeeded.",
-            result: await analytics.find().limit(6)
+            result: (await analytics.find().sort({CurrentMonth: -1}).limit(6)).reverse()
         })
     })
 });
